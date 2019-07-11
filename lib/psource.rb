47328b84298f6dfc809cfdb449edf600a56e4b9e
nishisuke <ryoskns+git@gmail.com>
2019-07-11 10:54:52 +0900

    add Method#source

--- lib/psource.rb
+++ lib/psource.rb
@@ -1,6 +1,19 @@
+# frozen_string_literal: true
+
 require "psource/version"
 
 module Psource
   class Error < StandardError; end
   # Your code goes here...
+
+  autoload :Source, 'psource/source'
+
+  module MethodExtension
+    def source
+      path, line = source_location
+      Source.new(path: path).definition_lines_begins_at(line)
+    end
+  end
 end
+
+Method.include(Psource::MethodExtension)
---
+++ lib/psource/source.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'psource/parser'
+
+module Psource
+  class Source
+    def initialize(path:, parser: Parser.new)
+      @path = path
+      @parser = parser
+    end
+
+    def definition_lines_begins_at(line)
+      beginning_index = line - 1
+      lines = File.readlines(path)
+      ending_index = (beginning_index..).find do |index|
+        code = lines[beginning_index..index].join
+        parser.parse(code)
+      end
+      lines[beginning_index..ending_index]
+    end
+
+    private
+
+    attr_reader :path, :parser
+  end
+end
